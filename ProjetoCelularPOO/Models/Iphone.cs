c1377edf04165f810f44b61107ff9583027e21b3
Peterson <petersoncabrini@hotmail.com>
2022-12-30 22:56:21 -0300

    Herdando de Smarthphone e sobrescrevendo o metodo InstalarAplicativo

---
+++ ProjetoCelularPOO/Models/Iphone.cs
@@ -0,0 +1,18 @@
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no IPhone");
+        }
+    }
+}
---
+++ ProjetoCelularPOO/Models/Nokia.cs
@@ -0,0 +1,18 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
+        }
+    }
+}

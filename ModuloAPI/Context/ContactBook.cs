1552f43bfd96db2ebf661203d35d2c05704d8aeb
Peterson <petersoncabrini@hotmail.com>
2023-01-08 17:10:50 -0300

    criando o contexto

---
+++ ModuloAPI/Context/ContactBook.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModuloAPI.Models;
+
+namespace ModuloAPI.Context
+{
+    public class ContactBook: DbContext
+    {
+        public ContactBook(DbContextOptions<ContactBook> options) : base (options){
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}

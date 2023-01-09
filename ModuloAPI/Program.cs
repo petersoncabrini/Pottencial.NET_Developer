f642fe8666d11baa10dd7bf2761ac1f48c8929a8
Peterson <petersoncabrini@hotmail.com>
2023-01-09 00:22:29 -0300

    configurando a conexao com o banco

--- ModuloAPI/Program.cs
+++ ModuloAPI/Program.cs
@@ -1,6 +1,13 @@
+using ModuloAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ContactBook>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

1dfa7a64dc3778f7ca2e642d6b9e04a5a3808a53
Peterson <petersoncabrini@hotmail.com>
2023-01-11 20:14:59 -0300

    Implementação dos métodos do CRUD

--- ModuloMVC/Controllers/ContactController.cs
+++ ModuloMVC/Controllers/ContactController.cs
@@ -35,5 +35,61 @@ namespace ModuloMVC.Controllers
 
             return View(contact);
         }
+
+        public IActionResult Edit(int id)
+        {
+            var contato = _context.Contacts.Find(id);
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+           
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            var dbContact = _context.Contacts.Find(contact.Id);
+            dbContact.Nome = contact.Nome;
+            dbContact.Telefone = contact.Telefone;
+            dbContact.Ativo = contact.Ativo;
+
+            _context.Contacts.Update(dbContact);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public IActionResult Details (int id)
+        {
+            var contato = _context.Contacts.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var contato = _context.Contacts.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact contact)
+        {
+            var dbContact = _context.Contacts.Find(contact.Id);
+
+            _context.Contacts.Remove(dbContact);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }

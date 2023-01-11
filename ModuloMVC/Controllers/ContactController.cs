6e3b70aa3cd8bba68c598d08885ce4de6894cef2
Peterson <petersoncabrini@hotmail.com>
2023-01-10 21:45:14 -0300

    adicionar view para criar contato

--- ModuloMVC/Controllers/ContactController.cs
+++ ModuloMVC/Controllers/ContactController.cs
@@ -1,14 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ModuloMVC.Context;
 
 namespace ModuloMVC.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly ContactBookContext _context;
+
+        public ContactController(ContactBookContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index(){
+            var contatos = _context.Contacts.ToList();
+            return View(contatos);
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
     }
